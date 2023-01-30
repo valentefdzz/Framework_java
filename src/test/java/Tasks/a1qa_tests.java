08ba87a49d6297054ac46005f81da1642f474259
valentefdzz <122126737+valentefdzz@users.noreply.github.com>
2023-01-29 20:29:39 -0600

    Second Commit

--- src/test/java/Tasks/a1qa_tests.java
+++ src/test/java/Tasks/a1qa_tests.java
@@ -3,6 +3,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({task_1.class})
+@Suite.SuiteClasses({task_1.class, task_2.class, task_3.class, task_4.class})
 public class a1qa_tests {
+
 }
